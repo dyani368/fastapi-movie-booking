b0eb92732b6c6195f59cc05eff3c7303997ce679
Dyani <dyanhawal@gmail.com>
2026-06-23 10:59:57 +0530

    migrated to postgreSQL

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_8kXUgRQpYi4E@ep-purple-mud-ah8a7e82.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    SQLALCHEMY_DATABASE_URL    
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
